27b0c850569ce10afd2b27e90301e86a9c21db4d
Mallhar03 <ptavinbawgikar1@gmail.com>
2025-11-28 17:29:37 +0530

    cors safety implemented

--- camply-backend/index.js
+++ camply-backend/index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000
 
 // Middleware      
 //bro i dont understand this......
-app.use(cors())
+app.use(cors(
+    {
+        origin :process.env.NODE_ENV === 'production' ? 'https://camply.live' : 'http://localhost:5173',
+        credentials : true
+    }
+))
 app.use(express.json())
 
 app.get('/api/posts', async (req, res) => {
